d483d88b0998415583ea5a2dfbc6c56968c10bac
Neonx <fabiangomes2015@gmail.com>
2023-07-15 13:49:20 -0500

    adding routing module

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# AngularSimpleCRUD
+# A
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.1.1.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.1.4.
 
 ## Development server
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './gastos/components/components.component';
 import { GastosComponent } from './components/gastos/gastos.component';
 import { IngresarPresupuestoComponent } from './components/ingresar-presupuesto/ingresar-presupuesto.component';
-import { IngresarGastoComponent } from './components/gastos/ingresar-gasto/ingresar-gasto.component';
+import { IngresarGastoComponent } from './gastos/ingresar-gasto/ingresar-gasto.component';
 import { ListasGastosComponent } from './components/gastos/listas-gastos/listas-gastos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ComponentsComponent,
     GastosComponent,
     IngresarPresupuestoComponent,
     IngresarGastoComponent,
     ListasGastosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
