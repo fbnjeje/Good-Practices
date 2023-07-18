1024de841d2ec403645c1e9fd2ea8d87e7e08a6f
fbn <fabiangomes2015@gmail.com>
2023-07-18 14:47:59 -0500

    Fixing imports of app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ComponentsComponent } from './gastos/components/components.component';
+
+
 import { GastosComponent } from './components/gastos/gastos.component';
 import { IngresarPresupuestoComponent } from './components/ingresar-presupuesto/ingresar-presupuesto.component';
-import { IngresarGastoComponent } from './gastos/ingresar-gasto/ingresar-gasto.component';
+
+import { IngresarGastoComponent } from './components/gastos/ingresar-gasto/ingresar-gasto.component';
 import { ListasGastosComponent } from './components/gastos/listas-gastos/listas-gastos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentsComponent,
     GastosComponent,
     IngresarPresupuestoComponent,
     IngresarGastoComponent,
