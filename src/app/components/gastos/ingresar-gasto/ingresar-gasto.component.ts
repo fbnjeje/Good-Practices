e6c567bd93151bb68f6e80fbd7f88f9c84496104
Neonx <fabiangomes2015@gmail.com>
2023-07-21 12:18:45 -0500

    Add validation of 'restante' valor

--- src/app/components/gastos/ingresar-gasto/ingresar-gasto.component.ts
+++ src/app/components/gastos/ingresar-gasto/ingresar-gasto.component.ts
@@ -1,3 +1,4 @@
+import { PresupuestoService } from 'src/app/services/presupuesto.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -11,18 +12,26 @@ cantidad:number;
 formularioIncorrecto:boolean;
 textIncorrecto:string;
 
-constructor(){
+constructor(private _presupuestoService:PresupuestoService){
   this.nombreGasto='';
   this.cantidad = 0;
   this.formularioIncorrecto = false
   this.textIncorrecto ='Nombre o gasto cantidad incorrecta'
 }
 agregarGasto(){
+
+
+if(this.cantidad > this._presupuestoService.restante){
+  this.formularioIncorrecto = true;
+  this.textIncorrecto= 'Cantidad ingresada es mayor al restante';
+}
+
   if(this.nombreGasto === '' || this.cantidad <= 0){
     this.formularioIncorrecto = true
   }else{
     this.formularioIncorrecto = false
-
+    this.nombreGasto = ''
+    this.cantidad = 0
   }
 }
 }
